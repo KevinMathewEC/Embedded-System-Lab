df52b8a7bad06f2a8dff3bd2331e67e1a0ac308a
KevinMathewEC <51322169+KevinMathewEC@users.noreply.github.com>
2024-01-24 22:11:43 +0530

    Update Led_Blinker_Tiva.c

--- Led_Blinker_Tiva.c
+++ Led_Blinker_Tiva.c
@@ -21,7 +21,12 @@
 #define COLOR_MAGNETA 0x06
 #define COLOR_WHITE 0x0E
 #define MAX_DELAY 500
+int unsigned state=0x00;
+int unsigned color=0x00;
+short all_on=0;
+int unsigned delay=MAX_DELAY;
 void delayMs(int n);
+void LED_off();
 int main(void)
 {
     SYSCTL_RCGCGPIO_R |= SYSCTL_RCGC2_GPIOF; // enable clock to GPIOF at clock gating
@@ -33,93 +38,95 @@ int main(void)
     GPIO_PORTF_DIR_R &= (~(GPIO_PORTF_PIN0_EN+GPIO_PORTF_PIN4_EN));
     GPIO_PORTF_PUR_R |= GPIO_PORTF_PIN0_EN+GPIO_PORTF_PIN4_EN;//Pull-up PF4(sw1) and PF0(sw0)
 
-
-//    SYSCTL_RCGC2_R |= 0x00000020; /* enable clock to GPIOF at clock gating
-//control register */
-//GPIO_PORTF_LOCK_R = 0x4C4F434B;//Unlock CR register
-//GPIO_PORTF_CR_R |= 0x01;//Enable pull_up on PF0
-//GPIO_PORTF_PUR_R |= 0x11;//Pull-up PF4(sw1) and PF0(sw0)
-//GPIO_PORTF_DIR_R |= 0x0E; /* enable the GPIO pins for the LED (PF3, 2
-//1) as output */
-//GPIO_PORTF_DEN_R |= 0x1F; /* enable the GPIO pins for digital function
-//*/
-
-int unsigned delay=MAX_DELAY;
-int unsigned state=0x00;
-int unsigned color=0x00;
-short all_on=0;
 while(1) {
-         if((GPIO_PORTF_DATA_RD & 0x10) == 0x00){
-             switch(state){
-               case 0x00:
-                   GPIO_PORTF_DATA_WR = COLOR_GREEN;
-                   state=state+0x01;
-               break;
 
-               case 0x01:
-                   GPIO_PORTF_DATA_WR = COLOR_BLUE;
-                   state=state+0x01;
-               break;
+    switch(state){
+              case 0x00:
+                  GPIO_PORTF_DATA_WR = COLOR_GREEN;
+                  delayMs(delay);
+              break;
 
-               case 0x02:
-                   GPIO_PORTF_DATA_WR = COLOR_CYAN;
-                   state=state+0x01;
-               break;
+              case 0x01:
+                  GPIO_PORTF_DATA_WR = COLOR_BLUE;
+                  delayMs(delay);
+              break;
 
-               case 0x03:
-                   GPIO_PORTF_DATA_WR = COLOR_RED;
-                   state=state+0x01;
-               break;
+              case 0x02:
+                  GPIO_PORTF_DATA_WR = COLOR_CYAN;
+                  delayMs(delay);
+              break;
 
-               case 0x04:
-                   GPIO_PORTF_DATA_WR = COLOR_YELLOW;
-                   state=state+0x01;
-               break;
+              case 0x03:
+                  GPIO_PORTF_DATA_WR = COLOR_RED;
+                  delayMs(delay);
+              break;
 
-               case 0x05:
-                   GPIO_PORTF_DATA_WR = COLOR_MAGNETA;
-                   state=state+0x01;
-               break;
+              case 0x04:
+                  GPIO_PORTF_DATA_WR = COLOR_YELLOW;
+                  delayMs(delay);
 
-               case 0x06:
-                   GPIO_PORTF_DATA_WR = COLOR_WHITE;
-                   state=state+0x01;
-               break;
-               default:
-                   state=0x00;
-               break;
-             }
-         }
-             else{
-                 GPIO_PORTF_DATA_WR =color;
-             }
+              break;
 
-        color=GPIO_PORTF_DATA_WR;
-        delayMs(delay);
-        if(!all_on){
-        GPIO_PORTF_DATA_WR = 0x00; /* turn off all LEDs */
-        delayMs(delay);
-        }
+              case 0x05:
+                  GPIO_PORTF_DATA_WR = COLOR_MAGNETA;
+                  delayMs(delay);
+              break;
 
-
-        if((GPIO_PORTF_DATA_RD & 0x01) == 0x00){
-            if(delay>=8){
-                delay=delay/2;
-                all_on=0;
+              case 0x06:
+                  GPIO_PORTF_DATA_WR = COLOR_WHITE;
+                  delayMs(delay);
+              break;
+              default:
+                  state=0x00;
+              break;
             }
-            else{
-                all_on=1;
-                delay=MAX_DELAY*2;
-            }
-
-        }
+    LED_off();
 
  }
 }
 /* delay n milliseconds (16 MHz CPU clock) */
+void switch_press()
+{
+    if((GPIO_PORTF_DATA_RD & 0x10) == 0x00){
+        for(int i = 0 ; i < 50; i++)
+            for(int j = 0; j < 3180; j++) {} /* do nothing for 1 ms */
+        if((GPIO_PORTF_DATA_RD & 0x10) == 0x00)
+            state=state+0x01;
+    }
+
+
+    if((GPIO_PORTF_DATA_RD & 0x01) == 0x00){
+        for(int i = 0 ; i < 50; i++)
+                    for(int j = 0; j < 3180; j++) {} /* do nothing for 1 ms */
+        if((GPIO_PORTF_DATA_RD & 0x01) == 0x00){
+         if(delay>=8){
+            delay=delay/2;
+            all_on=0;
+         }
+         else{
+            all_on=1;
+            delay=MAX_DELAY*2;
+         }
+        }
+    }
+
+}
+void LED_off(){
+    if(!all_on){
+    GPIO_PORTF_DATA_WR = 0x00; /* turn off all LEDs */
+    delayMs(delay);
+    }
+}
 void delayMs(int n)
 {
 int i, j;
-for(i = 0 ; i < n; i++)
+for(i = 0 ; i < n; i++){
+    switch_press();
 for(j = 0; j < 3180; j++) {} /* do nothing for 1 ms */
 }
+}
+
+
+
+
+
