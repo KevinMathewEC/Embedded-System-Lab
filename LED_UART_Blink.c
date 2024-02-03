428c434ee80b78911465ee3eeb9ef164c002e292
KevinMathewEC <51322169+KevinMathewEC@users.noreply.github.com>
2024-02-03 23:09:10 +0530

    Update LED_UART_Blink.c

--- LED_UART_Blink.c
+++ LED_UART_Blink.c
@@ -11,8 +11,15 @@
 #include <C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\pin_map.h>
 #include <C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\sysctl.h>
 #include <C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\uart.h>
+//# define SYSCTL_RCGCGPIO_R (*(( volatile unsigned long *)0x400FE608))
 # define GPIO_PORTF_DATA_RD (*(( volatile unsigned long *)0x40025044))
 # define GPIO_PORTF_DATA_WR (*(( volatile unsigned long *)0x40025038))
+//# define GPIO_PORTF_DIR_R (*(( volatile unsigned long *)0x40025400))
+//# define GPIO_PORTF_DEN_R (*(( volatile unsigned long *)0x4002551C))
+//# define GPIO_PORTF_PUR_R (*(( volatile unsigned long *)0x40025510 ))
+//# define GPIO_PORTF_LOCK_R (*((volatile unsigned long *)0x40025520))
+//# define GPIO_PORTF_CR_R  (*((volatile unsigned long *)0x40025524))
+//# define SYSCTL_RCGC2_GPIOF 0x0020
 # define GPIO_PORTF_PIN3_1_EN 0x0E
 # define GPIO_PORTF_PIN0_EN 0x01
 # define GPIO_PORTF_PIN4_EN 0x10
@@ -33,6 +40,7 @@
 int unsigned delay=MAX_DELAY;
 void delayMs(int n);
 void LED_off();
+void UART0_Transmitter(char);
 void UART0_init();
 void UART_cmpInp();//Compute data received from console
 void UART_InString(char* , uint16_t);
@@ -52,8 +60,8 @@ int length=0;
 int char_cnt=0;
 int main(void)
 {
-
-
+ 
+ 
     SYSCTL_RCGCGPIO_R |= SYSCTL_RCGC2_GPIOF; // enable clock to GPIOF at clock gating
     GPIO_PORTF_LOCK_R = 0x4C4F434B;//Unlock CR register
     GPIO_PORTF_CR_R |= 0x01;//Enable pull_up on PF0
@@ -62,46 +70,46 @@ int main(void)
 //1) as output */
    GPIO_PORTF_DIR_R &= (~(GPIO_PORTF_PIN0_EN+GPIO_PORTF_PIN4_EN));
    GPIO_PORTF_PUR_R |= GPIO_PORTF_PIN0_EN+GPIO_PORTF_PIN4_EN;//Pull-up PF4(sw1) and PF0(sw0)
-
+ 
     //UART
    UART0_init();
    UART_OutChar('>');
-
-
+ 
+ 
 while(1) {
-
+ 
     switch(state){
               case 0x00:
                   GPIO_PORTF_DATA_WR = COLOR_GREEN;
                    delayMs(delay);
               break;
-
+ 
               case 0x01:
                   GPIO_PORTF_DATA_WR = COLOR_BLUE;
                   delayMs(delay);
               break;
-
+ 
               case 0x02:
                   GPIO_PORTF_DATA_WR = COLOR_CYAN;
                   delayMs(delay);
               break;
-
+ 
               case 0x03:
                   GPIO_PORTF_DATA_WR = COLOR_RED;
                   delayMs(delay);
               break;
-
+ 
               case 0x04:
                   GPIO_PORTF_DATA_WR = COLOR_YELLOW;
                   delayMs(delay);
-
+ 
               break;
-
+ 
               case 0x05:
                   GPIO_PORTF_DATA_WR = COLOR_MAGNETA;
                   delayMs(delay);
               break;
-
+ 
               case 0x06:
                   GPIO_PORTF_DATA_WR = COLOR_WHITE;
                   delayMs(delay);
@@ -111,16 +119,16 @@ while(1) {
               break;
             }
     LED_off();
-
- }
+ 
+}
 }
 void UART_cmpInp()
 {
     short i=0;
     char cmd_string[10];
-
+ 
     UART_InString(string,MAX_INP_BUFF_SIZE);
-
+ 
     if(!inp_done){
         if(string[0] == BS){
             if(char_cnt)
@@ -132,29 +140,28 @@ void UART_cmpInp()
         valid_char=0;
         }
     }
-
+ 
    if(inp_done){
-
      while(console_cmd_buffer[i]!=CR){
          console_cmd_buffer[i] = tolower(console_cmd_buffer[i]);
         i++;
-
+ 
     }
     // Copy first 5 characters to first_part string
     strncpy(cmd_string, console_cmd_buffer, 5);
     cmd_string[5] = '\0'; // Null-terminate first_part string
-
+ 
    //  UART_OutString(token);
     typedef struct Color {
         char color_name[7];
         int state_value;
     } Color;
-
+ 
     Color colors[] = {{"green", 0}, {"blue", 1},\
                       {"cyan", 2} , {"red", 3},\
                       {"yellow", 4}, {"magenta", 5},\
                       {"white", 6}};
-
+ 
         if(!(strcmp(cmd_string,"color"))){
             strcpy(cmd_string, console_cmd_buffer + 5);
          OutCRLF();
@@ -162,7 +169,7 @@ void UART_cmpInp()
            if (strcmp(cmd_string, colors[i].color_name) == 0) {
              state = colors[i].state_value;
 
-
+ 
     }
    }
   }
@@ -200,10 +207,10 @@ void UART0_init()
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), BAUD_RATE,(UART_CONFIG_WLEN_8\
                         | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-
+ 
 }
-
-
+ 
+ 
 void OutCRLF(void){
   UART_OutChar(CR);
   UART_OutChar(LF);
@@ -216,7 +223,7 @@ void UART_OutChar(unsigned char data){
   while((UART0_FR_R&UART_FR_TXFF) != 0);
   UART0_DR_R = data;
 }
-
+ 
 void UART_OutString(char *pt){
   char* ptr = pt;
   while(*ptr){
@@ -225,11 +232,11 @@ void UART_OutString(char *pt){
   }
 }
 void UART_InString(char *bufPt, uint16_t max) {
-
+ 
 char character;
-
- character = UART_InChar();
-
+ 
+character = UART_InChar();
+ 
   if(character != CR){
     if(character == BS){
       if(length){
@@ -254,7 +261,7 @@ char character;
       bufPt++;
       length++;
       UART_OutChar(character);
-
+ 
     }
     else{
         valid_char=0;
@@ -265,12 +272,11 @@ char character;
   else{
   *bufPt = 0;
   inp_done=1;
-
+ 
   }
- }
-
-
-
+}
+ 
+ 
 /* delay n milliseconds (16 MHz CPU clock) */
 void switch_press()
 {
@@ -279,18 +285,17 @@ void switch_press()
         switch_press=1;
         Debounce_Delay();
     }
-
+ 
     if(switch_press==1){
-
             state=state+0x01;
             switch_press=0;
     }
-
+ 
     while((GPIO_PORTF_DATA_RD & 0x01) == 0x00){
         switch_press=2;
         Debounce_Delay();
     }
-
+ 
     if(switch_press==2){
         switch_press=0;
          if(delay>=8){
@@ -303,14 +308,14 @@ void switch_press()
          }
         }
     //}
-
+ 
 }
 void Clear_InpBuff(){
     for(int i=0;i<MAX_INP_BUFF_SIZE;i++)
         console_cmd_buffer[i]=0;
 }
 void LED_off(){
-
+ 
     if(!all_on){
        GPIO_PORTF_DATA_WR = 0x00; /* turn off all LEDs */
     delayMs(delay);
@@ -331,8 +336,3 @@ void Debounce_Delay()
     for(int i = 0 ; i < 50; i++)
                 for(int j = 0; j < 3180; j++) {} /* do nothing for 1 ms */
 }
-
-
-
-
-
