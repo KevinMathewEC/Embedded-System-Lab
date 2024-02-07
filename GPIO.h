4aae0762b1b25bf8f5b86c3ee97ac3c778dc47a6
KEVIN MATHEW <51322169+KevinMathewEC@users.noreply.github.com>
2024-02-07 21:38:33 +0530

    Create GPIO.h

---
+++ GPIO.h
@@ -0,0 +1,27 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: 06-Feb-2024
+ *      Author: Kevin
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\tm4c123gh6pm.h"
+
+void GPIOA_INIT(void);
+
+void GPIOB_INIT(void);
+
+void GPIOC_INIT(void);
+
+void GPIOD_INIT(void);
+
+void GPIOE_INIT(void);
+
+void GPIOF_INIT(void);
+
+
+#endif /* GPIO_H_ */
