036f7f6aed49256347af985e06841cee2dcdc0c1
KEVIN MATHEW <51322169+KevinMathewEC@users.noreply.github.com>
2024-02-07 21:39:21 +0530

    Create KEYPAD.h

---
+++ KEYPAD.h
@@ -0,0 +1,33 @@
+/*
+ * KEYPAD.h
+ *
+ *  Created on: 06-Feb-2024
+ *      Author: Kevin
+ */
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\tm4c123gh6pm.h"
+
+
+
+
+#endif /* KEYPAD_H_ */
+
+
+void keypad_init(void);
+
+int isKeyPressed(void);
+
+int readkey(void);
+
+void delay_1_Ms(void);
+
+
+
+
+#endif /* KEYPAD_H_ */
